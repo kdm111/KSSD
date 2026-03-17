437fb87545ec3d8e575a7a855123b8f359a43909
kdm111 <pjsu94@naver.com>
2026-03-17 16:50:29 +0900

    add app2

---
+++ App2/DB/repository.py
@@ -0,0 +1,84 @@
+import time
+from .models import *
+from datetime import datetime
+
+# log
+def insert_log(gesture: str):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"[{timestamp}] {gesture} 입력")
+    Log.create(timestamp=timestamp, gesture=gesture, operation = 'recognize')
+
+def get_recent(limit: int = 100):
+    return Log.select().order_by(Log.timestamp.desc()).limit(limit)
+
+def get_all():
+    return Log.select()
+
+# gesture
+# insert
+def insert_gesture_log(gesture: str, action: str):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"GestureTable : [{timestamp}] {gesture} 입력")
+    Gesture.create(timestamp=timestamp,gesture=gesture,action_map=action)
+    Log.create(timestamp=timestamp,gesture=gesture,operation='insert')
+
+# delete
+def delete_gesture_log(gesture: str):
+    query = Gesture.delete().where(Gesture.gesture == gesture)
+    query.execute() # 실제로 삭제 실행
+
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    print(f"GestureTable : [{timestamp}] {gesture} 삭제 완료")
+    Log.create(timestamp=timestamp,gesture=gesture,operation='delete')
+
+# update
+def update_gesture_log(old_gesture:str,new_gesture:str):
+    query = Gesture.update(gesture=new_gesture).where(Gesture.gesture == old_gesture)
+    query.execute() # 실제 업데이트 실행
+
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"GestureTable : [{timestamp}] 업데이트 완료")
+    Log.create(timestamp=timestamp,gesture=old_gesture,operation='update')
+
+# id값 가져오기
+def get_gesture_id(gesture:str):
+    try:
+        # gesture가 일치하는 데이터 중 하나를 가져옴
+        record = Gesture.select(Gesture.id).where(Gesture.gesture == gesture).get()
+        return record.id
+    except Gesture.DoesNotExist:
+        # 만약 해당 제스처가 DB에 없다면 처리
+        return None
+
+# action_map 가져오기
+def get_gesture_action(gesture:str):
+    try:
+        record = Gesture.select(Gesture.action_map).where(Gesture.gesture == gesture).get()
+        return record.action_map
+    except Gesture.DoesNotExist:
+        # 만약 해당 제스처가 DB에 없다면 처리
+        return None
+
+# DB안에있는 gesture 확인
+def check_gesture_exists(gesture:str) -> bool:
+    exists = Gesture.select().where(Gesture.gesture == gesture).exists()
+    return exists
+
+def get_gesture_recent(limit: int = 100):
+    return Gesture.select().order_by(Gesture.timestamp.desc()).limit(limit)
+
+def get_gesture_all():
+    return Gesture.select()
+
+
+# device log insert
+
+def insert_vehicle_state_log(state_dict):
+    return VehicleStateLog.create(
+        is_connected=state_dict.get("is_connected"),
+        status=state_dict.get("status"),
+        speed=state_dict.get("speed"),
+        current_command=state_dict.get("current_command"),
+        timestamp=state_dict.get("timestamp") # 전달받은 시간 사용
+    )
